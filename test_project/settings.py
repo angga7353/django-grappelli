62741ff96fe34d30312439490d9d09738ae46efa
angga7353 <47376072+angga7353@users.noreply.github.com>
2019-02-06 12:04:01 +0700

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -17,11 +17,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'grappelli',
 ]
 
